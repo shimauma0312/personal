6b6eb4b462b456bd7ef3e7cd7767c6dbfd19d3e0
shimauma <86759249+shimauma0312@users.noreply.github.com>
2025-02-21 05:22:11 +0000

    Initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: [
+    '@/assets/main.css'
+  ],
+  build: {
+    transpile: ['@element-plus/icons-vue']
+  },
+  modules: [
+    '@element-plus/nuxt'
+  ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-container style="height: 100%">
+    <!--サイドメニュー-->
+    <SideMenu />
+    <!--メインコンテンツ-->
+    <el-container>
+      <el-header class="line">
+        <h1>Element plus検証用ページ</h1>
+        <el-icon>
+          <Shop />
+        </el-icon>
+      </el-header>
+      <el-main>
+        <div>
+          <p>これはVue 3で作成されたホームページです。</p>
+        </div>
+        <el-button type="primary">Default</el-button>
+      </el-main>
+      <!--<el-footer>aaa</el-footer>-->
+    </el-container>
+  </el-container>
+</template>
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+header h1 {
+  color: rgb(185, 123, 66);
+}
+
+header {
+  width: 100%;
+  /* --el-border-color */
+  border-bottom: 1px solid var(--el-border-color);
+}
+</style>
