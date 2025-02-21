08c918b89f5eae2b6e9013d6a386ea88cb4a9a93
shimauma <86759249+shimauma0312@users.noreply.github.com>
2025-02-21 05:51:48 +0000

    コメント

--- plugins/element-plus-icons.ts
+++ plugins/element-plus-icons.ts
@@ -2,7 +2,9 @@ import { defineNuxtPlugin, NuxtApp } from '#app'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 export default defineNuxtPlugin((nuxtApp: NuxtApp) => {
+    // ElementPlusIconsVueの各コンポーネントをループで登録する
     for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        // コンポーネントをNuxtアプリケーションに登録する
         nuxtApp.vueApp.component(key, component)
     }
 })
